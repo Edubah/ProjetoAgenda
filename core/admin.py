2b96bf920dd1cf5892a79f2995db4eec97c6c3be
Eduardo <edugc20142hotmail.com>
2022-07-01 15:44:06 -0300

    Projeto Agenda 1.5

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from core.models import Eventos
 # Register your models here.
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'data_evento', 'data_criacao')
+
+
+admin.site.register(Eventos, EventoAdmin)
